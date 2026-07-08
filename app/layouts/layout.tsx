c181ac8557d00729504d4595ffc49c16d1d01590
Jonil <jonilsarmiento4@gmail.com>
2026-07-08 14:36:12 +0800

    Edit layout.tsx

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Footer from "~/components/Footer";
-import Header from "~/components/header";
+import Header from "~/components/Header";
 
 export default function Layout() {
       return (
